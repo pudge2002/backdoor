bfef1fb98594c50bc595eae5a2e37a90e44b460b
pudge2002 <silverhof634@gmail.com>
2024-11-16 13:32:06 +0300

    add openProdView

--- src/main/java/com/example/backdoor/views/OpenProduct.java
+++ src/main/java/com/example/backdoor/views/OpenProduct.java
@@ -1,9 +1,9 @@
 package com.example.backdoor.views;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
@@ -12,114 +12,110 @@ import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 
-import com.example.backdoor.model.*;
-
-import com.example.backdoor.repos.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 
-import java.awt.*;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-@PageTitle("Список продуктов")
-@Route("")
-@Menu(order = 0, icon = "line-awesome/svg/pencil-ruler-solid.svg")
+@PageTitle("Продукт")
+@Route("op")
+@Menu(order = 1, icon = "line-awesome/svg/pencil-ruler-solid.svg")
 @Uses(Icon.class)
-public class HomeView extends Composite<VerticalLayout> {
+public class   OpenProduct extends Composite<VerticalLayout> {
 
-    private final PropertyValueRepository propertyValueRepository;
-    private final RoleRepository roleRepository;
-
-    public HomeView(PropertyValueRepository propertyValueRepository, RoleRepository roleRepository) {
-        this.propertyValueRepository = propertyValueRepository;
-        this.roleRepository = roleRepository;
+    public   OpenProduct() {
         FormLayout formLayout2Col = new FormLayout();
-
-        Grid<PropertyValue> basicGrid = new Grid<>(PropertyValue.class);
-        Grid<Role> basicGrid2 = new Grid<>(Role.class);
-
+        Grid basicGrid = new Grid();
+        Grid basicGrid2 = new Grid();
         HorizontalLayout layoutRow = new HorizontalLayout();
         //<theme-editor-local-classname>
         layoutRow.addClassName("home-view-horizontal-layout-1");
-        Button buttonPrimary = new Button();
+        Button options = new Button();
         //<theme-editor-local-classname>
-        buttonPrimary.addClassName("home-view-button-1");
+        options.addClassName("home-view-button-1");
         Button buttonPrimary2 = new Button();
         //<theme-editor-local-classname>
         buttonPrimary2.addClassName("home-view-button-2");
         Button buttonPrimary3 = new Button();
         //<theme-editor-local-classname>
         buttonPrimary3.addClassName("home-view-button-3");
-        Button buttonPrimary4 = new Button();
+        Button addRisk = new Button();
         //<theme-editor-local-classname>
-        buttonPrimary4.addClassName("home-view-button-4");
+        addRisk.addClassName("home-view-button-4");
         HorizontalLayout layoutRow2 = new HorizontalLayout();
         //<theme-editor-local-classname>
         layoutRow2.addClassName("home-view-horizontal-layout-2");
-        Button buttonPrimary5 = new Button();
+        Button delRisk = new Button();
         //<theme-editor-local-classname>
-        buttonPrimary5.addClassName("home-view-button-5");
+        delRisk.addClassName("home-view-button-5");
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
         formLayout2Col.setWidth("100%");
-        H2 header = new H2("Список продуктов");
+        H2 header = new H2("Стратегии");
         H2 grid1Header = new H2("Список ролей");
         //<theme-editor-local-classname>
         grid1Header.addClassName("home-view-h2-1");
         basicGrid.setWidth("1000px");
         basicGrid.getStyle().set("flex-grow", "0");
-        setGridSampleData_product(basicGrid);
+        setGridSampleData(basicGrid);
         basicGrid2.setWidth("250px");
         basicGrid2.getStyle().set("flex-grow", "0");
-        setGridSampleData_roles(basicGrid2);
+        setGridSampleData(basicGrid2);
         layoutRow.setHeightFull();
         layoutRow.addClassName(Gap.MEDIUM);
         layoutRow.setWidth("100%");
         layoutRow.getStyle().set("flex-grow", "1");
-        buttonPrimary.setText("Добавить");
-        buttonPrimary.setWidth("min-content");
-        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        options.setText("Настройки");
+        options.setWidth("min-content");
+        options.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         buttonPrimary2.setText("Открыть");
         buttonPrimary2.setWidth("min-content");
         buttonPrimary2.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         buttonPrimary3.setText("Настройки");
         buttonPrimary3.setWidth("min-content");
         buttonPrimary3.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        buttonPrimary4.setText("Редактирование");
-        buttonPrimary4.setWidth("min-content");
-        buttonPrimary4.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        addRisk.setText("Привязать");
+        addRisk.setWidth("min-content");
+        addRisk.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         layoutRow2.setHeightFull();
         layoutRow2.addClassName(Gap.MEDIUM);
         layoutRow2.setWidth("100%");
         layoutRow2.getStyle().set("flex-grow", "1");
-        buttonPrimary5.setText("Выбрать роль");
-        buttonPrimary5.setWidth("min-content");
-        buttonPrimary5.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        delRisk.setText("Отвязать");
+        delRisk.setWidth("min-content");
+        delRisk.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        List<String> prod = new ArrayList<>(Arrays.asList("ржака", "ахахахахd"));
+        ComboBox allProducts = new ComboBox("Выбор продукта", prod);
+        TextField name = new TextField();
+        TextField typeStrah =  new TextField();
+        name.setLabel("Наименование");
+        typeStrah.setLabel("Вид страхования");
+
+        VerticalLayout updateProd = new VerticalLayout(allProducts,name, typeStrah);
+        updateProd.add(name);
+        getContent().add(updateProd);
         getContent().add(formLayout2Col);
-
-        formLayout2Col.add( header, grid1Header, basicGrid, basicGrid2, layoutRow);
-
-        layoutRow.add(buttonPrimary);
-        layoutRow.add(buttonPrimary2);
-        layoutRow.add(buttonPrimary3);
-        layoutRow.add(buttonPrimary4);
+        formLayout2Col.add(header, grid1Header, basicGrid, basicGrid2, layoutRow);
+        layoutRow.add(options);
         formLayout2Col.add(layoutRow2);
-        layoutRow2.add(buttonPrimary5);
+        layoutRow2.add(addRisk,delRisk);
+        HorizontalLayout hl = new HorizontalLayout();
+        hl.add(new H1("dsdsds"));
     }
 
-    private void setGridSampleData_product(Grid grid) {
-        grid.setItems(propertyValueRepository.findAll());
+    private void setGridSampleData(Grid grid) {
+        //        grid.setItems(query -> samplePersonService.list(
+        //                        PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
+        //                .stream());
     }
-
-    private void setGridSampleData_roles(Grid grid) {
-        grid.setItems(roleRepository.findAll());
-    }
-
 }
