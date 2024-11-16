57ca0f206c20d4bbafc5c858891a1fef75f14832
pudge2002 <silverhof634@gmail.com>
2024-11-16 11:18:57 +0300

    view

--- src/main/java/com/example/backdoor/BackdoorApplication.java
+++ src/main/java/com/example/backdoor/BackdoorApplication.java
@@ -2,12 +2,14 @@ package com.example.backdoor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
-public class BackdoorApplication {
+@Theme("my-theme")
+public class BackdoorApplication implements AppShellConfigurator {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackdoorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackdoorApplication.class, args);
+    }
 }
