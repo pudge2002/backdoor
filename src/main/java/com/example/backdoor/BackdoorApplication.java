8f17f4bcbf24aa2447b3925d4f5f4fe00300dcaf
pudge2002 <silverhof634@gmail.com>
2024-11-16 10:48:45 +0300

    create proj

---
+++ src/main/java/com/example/backdoor/BackdoorApplication.java
@@ -0,0 +1,13 @@
+package com.example.backdoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackdoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackdoorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backdoor/BackdoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backdoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackdoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
